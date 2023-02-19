70c33f3a7702dbe2f124f1666445dfb0035e2c5f
ianrich69420 <ian.orieko@student.moringaschool.com>
2023-02-20 02:37:54 +0300

    Completed assignment

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,28 @@
-def roll_call_dwarves# code an argument here
+def roll_call_dwarves array# code an argument here
   # Your code here
+  array.each.with_index { |value, index| puts "#{index + 1}. #{value}"  }
 end
 
-def summon_captain_planet# code an argument here
+def summon_captain_planet array# code an argument here
   # Your code here
+  array.map { |value| "#{value.capitalize}!"}
 end
 
-def long_planeteer_calls# code an argument here
+def long_planeteer_calls array# code an argument here
   # Your code here
+  if array.find { |value| value.length > 4 } != nil
+    return true
+  else
+    return false
+  end
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese array# code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+  if array.find { |value| value == "cheddar" && value = "gouda" && value = "camembert" } == nil
+    return nil
+  else
+    return array.find { |value| value == "cheddar" && value = "gouda" && value = "camembert" }
+  end
 end
